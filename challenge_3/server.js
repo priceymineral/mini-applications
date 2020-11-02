18c802e39ef6d7cbd50f85bcea5f24678b79f052
Octavio <tudaeva@gmail.com>
2020-11-02 15:24:57 -0800

    installed nodemon and express, started working on the server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = '3000';
 
+// express.static(path.join(__dirname, 'public'));
+
+app.use('/checkout', express.static(path.join(__dirname, 'public')));
+
+app.listen('3000', () => {
+  console.log(`Listening on PORT http://localhost:${port}`);
+})
 
