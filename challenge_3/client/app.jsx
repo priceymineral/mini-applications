56e8eb9362987ea6741b9eb421a23ff5788a684b
Octavio <tudaeva@gmail.com>
2020-11-02 11:40:34 -0800

    challenge_3 environment is setup, react is rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+ReactDOM.render(<div>Hello Wurld!</div>, document.getElementById('app'))
 
---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js|jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development' 
+};
+
+
