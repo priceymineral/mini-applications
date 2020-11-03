094e5a827ea190ba9a6e9c01636f5f777725b53c
Octavio <tudaeva@gmail.com>
2020-11-03 15:36:33 -0800

    made server folder and moved server.js into it, still rendering correctly

--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 const port = '3000';
 
-// express.static(path.join(__dirname, 'public'));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.listen('3000', () => {
   console.log(`Listening on PORT http://localhost:${port}`);
