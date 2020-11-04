7ba6da13996e6a5688d401ad78ec9e8410350a55
Octavio <tudaeva@gmail.com>
2020-11-03 20:30:06 -0800

    game can be played, working on determining game outcome

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,8 +1,64 @@
+// GAME FUNCTIONALITY
+
+var box = document.getElementsByTagName('TD');
+// box is an array of objects
+console.log('each box:', box);
+var rows = document.getElementsByTagName('TR');
+console.log('rows:', rows);
+var table = document.getElementById('table');
+console.log('table:', table);
+
+// add eventlistener to every tr element
+
+for (var i = 0; i < box.length; i++) {
+  console.log('in loop');
+  box[i].addEventListener('click', modifyText, false);
+}
 
 
-console.log('Hello wassup');
+var arrayX = [];
+var arrayO = [];
+var count = 0;
+
+function modifyText() {
+  if (!count) {
+    this.firstChild.nodeValue = 'X';
+    count++;
+    arrayX.push(this.cellIndex);
+  } else if (count % 2 !== 0) {
+    this.firstChild.nodeValue = 'O';
+    count++;
+    arrayO.push(this.cellIndex);
+  } else {
+    this.firstChild.nodeValue = 'X';
+    count++;
+    arrayX.push(this.cellIndex);
+  }
+  console.log(arrayX, arrayO);
+}
 
 
-var boxed = document.getElementsByTagName('TD');
-console.log(boxed);
 
+// RESET BUTTON
+
+var resetButton = document.getElementById('button');
+function reset () {
+  for (var i = 0; i < box.length; i++) {
+    console.log('...')
+    box[i].firstChild.nodeValue = '*';
+    count = 0;
+    arrayX = [];
+    arrayO = [];
+  }
+}
+resetButton.addEventListener('click', reset, false);
+
+
+
+
+  // if (rows[0].outerText === 'XX  X') {
+  //   console.log('X Wins!!!');
+  // }
+  // console.log('XX  X => ', rows[0].outerText);
+  // console.log(typeof(rows[0].outerText));
+  // console.log(rows[0].outerText.length)
